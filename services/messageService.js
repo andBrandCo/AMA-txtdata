23459c8e180d53bade07c17b3c5563ec6799d78d
andBrandCo <github@andbrand.co>
2019-10-09 14:11:10 -0500

    added string response

--- services/messageService.js
+++ services/messageService.js
@@ -40,9 +40,8 @@ const findByKeyword = async (keyword, mobileNumber, res) => {
     });
     const twiml = new MessagingResponse();
     console.log(autoResponse)
-    //twiml.message(autoResponse);
-    twiml.message('The Robots are coming! Head for the hills!');
-    res.writeHead(200, { "Content-Type": "text/xml" });
+    twiml.message(autoResponse);
+    res.writeHead(200, {'Content-Type': 'text/xml'});
     res.end(twiml.toString());
     // testSendRequestToTwilio(phoneData.mobileNumber, autoResponse);
   }
