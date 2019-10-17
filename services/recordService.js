23ed64bfb98d954c9956d4d6560e5ed179f09b47
andBrandCo <github@andbrand.co>
2019-10-17 14:13:15 -0500

    added uid

--- services/recordService.js
+++ services/recordService.js
@@ -15,7 +15,15 @@ class RecordService {
 
     const request = new allRequest(data);
     console.log("new request - ", request);
-    //delete data._id;
+    request.delete();
+
+    delete data._id;
+
+    // Do the upsert, which works like this: If no Contact document exists with 
+    // _id = contact.id, then create a new doc using upsertData.
+    // Otherwise, update the existing doc with upsertData
+    request.update({_id: data._id}, data, {upsert: true});
+    //await request
     await request.save();
     console.log(request);
     return request;
